1e53f1a61d1cebbf5703de8b80726178929ae84c
deividi47 <vitinrf708@gmail.com>
2025-09-22 22:36:52 -0300

    mais exercicios

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//let nome = "Davi"
+
+
+function entrar(){
+
+    let area = document.getElementById("area");
+    let texto = prompt('Digite seu nome');
+
+    if(texto == '' || texto == null){
+        alert('Digite seu nome novamente')
+        area.innerHTML = 'Bem Vindo ...'
+    }else{
+        area.innerHTML = 'bem vindo ' + texto
+    }   
+}
+
+function entrar2(nome){
+    let area = document.getElementById("area");
+    let texto = prompt('Digite seu sobrenome');
+    area.innerHTML = nome + " " + texto
+}
