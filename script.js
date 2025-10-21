aae69b0d828f3b926291b7e3f949bf28c33125e1
deividi47 <vitinrf708@gmail.com>
2025-10-21 10:40:25 -0500

    aulas

--- script.js
+++ script.js
@@ -1,32 +1,9 @@
+let nome = '';
 
-
-
-function pedir(){
-    const cardapio = {
-        0: "Sorvete",
-        1: "Suco",
-        2: "Coca-Cola",
-        3: "Agua gelada"
-    };
-
-    let pedido = prompt("Digite o número do seu pedido:\n0 - Sorvete\n1 - Suco\n2 - Coca-Cola\n3 - Água gelada");
-    let pedidoContado = Number(pedido)
-
-    if(isNaN(pedidoContado)){
-        alert("Por favor, insira um numero valido.")
-        return;
-    }
-        
-    
-    switch(pedidoContado){
-
-        case 0:
-        case 1: 
-        case 2:
-        case 3:
-            alert(`Voce pediu um(a) ${cardapio[pedidoContado]}.`)
-            break;
-        default:
-        alert("Ops! Não temos essa opção no cardapio")
-    }
+if ( typeof localStorage.nome == 'undefined') {
+  localStorage.nome = prompt("Digite seu nome:");
 }
+
+nome = localStorage.nome
+
+document.getElementById('nome').innerHTML = nome;
