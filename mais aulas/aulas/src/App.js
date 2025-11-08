5b8b254b89b4b7acfe1f071b4d2938620fd311c1
deividi47 <vitinrf708@gmail.com>
2025-11-08 11:30:01 -0500

    exercicios

--- mais aulas/aulas/src/App.js
+++ mais aulas/aulas/src/App.js
@@ -1,34 +1,55 @@
-import React, { Component } from 'react';
-import Feed from './Components/Feed'
-class App extends Component{
-    
+import React, { Component }from 'react'
+
+class Feed extends Component{
     
     constructor(props){
-    super(props);
-    this.state = {
-      feed:[
-          {id: 1, username: 'Matheus', curtidas:10, comentarios: 2},
-          {id: 2, username: 'Lucas', curtidas:120, comentarios: 24},
-          {id: 3, username: 'Amanda', curtidas:30, comentarios: 12},
-          {id:4, username: 'Ricardo', curtidas:1, comentarios:12}
-      ]
+        super(props);
+        this.state = {
+         nome: "",
+         email: "",
+         senha: "",
+         error: ""
+        }
+        this.cadastrar = this.cadastrar.bind(this)
     }
-  }
+
+    cadastrar(event){
+      const {nome, email, senha} = this.state;
+      
+      if(nome !== "" && email !== "" && senha !== ""){
+        alert(`Nome: ${nome} \nEmail ${email} \nSenha: ${senha}`)
+
+      }else{
+        this.setState({error: 'Ops, parece que ouve algum erro'})
+      }
+        
+
+      event.preventDefault();
+    }
+
 
     render(){
         return(
             <div>
-               
-            {this.state.feed.map((item)=>{
-              return(
-               <Feed key={item.id} username={item.username}
-               curtidas={item.curtidas} comentarios={item.comentarios}/>
-              );
-            })}
+              <h1>Novo usuario</h1>
+            {this.state.error && <p>{this.state.error}</p>}
+              <form onSubmit={this.cadastrar}>
+                <label>Nome:</label>
+                <input type='text' value={this.state.nome} 
+                  onChange={(e) => this.setState({nome: e.target.value})}/> <br/>
 
+                <label>Email:</label>
+                <input type="email" value={this.state.email} onChange={(e) => this.setState({email: e.target.value})}/>
+                <br/>
+                <label>Senha:</label>
+                <input type="email" value={this.state.senha} onChange={(e) => this.setState({senha: e.target.value})}/>
+                <button type='submit'>Cadastrar</button>
+              </form>
             </div>
+
+
         )
     }
 }
 
-export default App
+export default Feed
